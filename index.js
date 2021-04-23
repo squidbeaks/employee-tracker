cf8e65bb44b3a92b454cf115c6334469fe4dc0e0
Leah Russell <leahsigridrussell@gmail.com>
2021-04-23 00:18:48 -0700

    Adding role functionality and queries

--- index.js
+++ index.js
@@ -37,12 +37,64 @@ const viewRoles = () => {
                 departments.name AS Department_Name,
                 salary AS Salary
                 FROM roles
-                INNER JOIN departments ON roles.department_id = departments.id;`
+                LEFT JOIN departments ON roles.department_id = departments.id;`
 
   db.query(sql, (err, rows) => {
     console.table(rows);
   });
 };
+
+const addRoles = () => {
+  const rolesSql = `SELECT
+              departments.name
+              FROM roles
+              LEFT JOIN departments ON roles.department_id = departments.id;`;
+
+  const choices = [];
+
+  db.query(rolesSql, (err, rows) => {
+    for (let i = 0; i < rows.length; i++) {
+      if (choices.indexOf(rows[i].name) === -1) {
+        choices.push(rows[i].name);
+      }
+    }
+  });
+
+  inquirer.prompt([
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is the title of the role you would like to add?'
+    },
+    {
+      type: 'number',
+      name: 'salary',
+      message: 'How much is the salary for this role?'
+    },
+    {
+      type: 'list',
+      name: 'department',
+      message: 'What department does this role belong to?',
+      choices: choices
+    }
+  ])
+  .then(responses => {
+    const departmentIdSql = `SELECT id FROM departments WHERE name = '${responses.department}';`
+    let department_id;
+
+    db.query(departmentIdSql, (err, rows) => {
+      department_id = rows[0].id;
+
+      const sql = `INSERT INTO roles (title, salary, department_id) VALUES (?,?,?);`
+      const params = [`${responses.title}`, responses.salary, department_id];
+
+      db.query(sql, params, (err, rows) => {
+        console.log(rows);
+        console.log(`${responses.title} successfully added to Roles database!`);
+      })
+    });
+  });
+}
 
 const viewEmployees = () => {
   const sql = `SELECT
@@ -94,7 +146,7 @@ const promptUser = () => {
     }
 
     if (response.userChoices === 'Add a role') {
-      console.log('Add a role');
+      addRoles();
     }
 
     if (response.userChoices === 'Add an employee') {
